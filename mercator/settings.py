8bc727ee3c16076f98057938944c95b872318a6d
val-h <valentin97mirchev@gmail.com>
2021-07-05 04:28:47 +0100

    users app started, mockup, custom user initialization, base urls

--- mercator/settings.py
+++ mercator/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# My additions
+AUTH_USER_MODEL = 'users.CustomUser'
---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def register(request):
+    """REgister view for our app."""
+    pass
+
+
+def login(request):
+    """Login view for our app."""
+    pass
+
+
+def logout(request):
+    """Logout view for our app."""
+    pass
