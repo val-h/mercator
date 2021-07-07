afe0fa07655291727b92650080f78ef3adaaec4e
val-h <valentin97mirchev@gmail.com>
2021-07-07 14:38:57 +0100

    user tests, password failure, fix it after work

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.import get_user_model
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserChangeForm, CustomUserCreateForm
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='test',
+            password='testpass123',
+            email='test@test.com'
+        )
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.password, 'testpass123')
+        self.assertEqual(user.email, 'test@test.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        su = User.objects.create_superuser(
+            username='su_test',
+            password='suTestPass@5',
+            email='su@admin.com'
+        )
+        self.assertEqual(su.username, 'su_test')
+        self.assertEqual(su.password, 'suTestPass@5')
+        self.assertEqual(su.email, 'su@admin.com')
+        self.assertTrue(su.is_active)
+        self.assertTrue(su.is_staff)
+        self.assertTrue(su.is_superuser)
