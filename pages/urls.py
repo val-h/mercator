19cc9a762e4ed61b585200ccd02640870f12a12c
val-h <valentin97mirchev@gmail.com>
2021-07-12 06:11:44 +0100

    I would not say better styling but rather having styling, responsive design, cart and search pages, added some dummy text to contact and about, created a better logo - maybe, some other stuff

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('privacy/', views.privacy, name='privacy'),
+    path('cart/', views.cart, name='cart'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,10 +8,13 @@ def categories(request):
     return render(request, 'categories.html', context={})
 
 def about(request):
-    return render(request, 'about.html', context={})
+    return render(request, 'about.html')
 
 def contact(request):
-    return render(request, 'contact.html', context={})
+    return render(request, 'contact.html')
 
 def privacy(request):
     return render(request, 'privacy.html')
+
+def cart(request):
+    return render(request, 'cart.html', context={})
